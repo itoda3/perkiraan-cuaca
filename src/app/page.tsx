d21cb91ffab90d074924202034589e1766c7e146
infolahta smatn <>
2025-06-17 11:30:21 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 // app/page.tsx
-'use client'; // Tandai sebagai Client Component
+'use client';
 
-// Import komponen WeatherDisplay dari path relatifnya
 import WeatherDisplay from './components/WeatherDisplay';
 
 export default function Home() {
   return (
-    // Anda cukup merender komponen WeatherDisplay di sini
-    // Semua logika cuaca, state, dan UI sudah ada di dalam WeatherDisplay
     <WeatherDisplay />
   );
 }
